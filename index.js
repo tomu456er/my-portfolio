6e5ae5ef6ace2b7d5802c6eb57377dced9c8ffb7
tomu456er <nagai.1040kamekawaii@icloud.com>
2025-03-05 16:47:25 +0900

    test

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("baz").addEventListener("click", function(){
+  alert(`Thank you!!`);
+});
+document.getElementById("foo").addEventListener("click", function(){
+  alert(`click more`);
+  alert(`have a nice day`);
+});
